5bfb4565e3113e0e9bfeb8a720b3ff4932f64d2f
Jonas Wilhelm <jonas.wilhelm@mr.mpg.de>
2022-02-23 20:48:47 +0100

    added function to read biokine ascii files

--- R/read_biokine.R
+++ R/read_biokine.R
@@ -2,11 +2,18 @@
 #'
 #' This function allows you to import bio-kine ascii files (.bka). 
 #' These are the standard output of BioLogic stopped flow devices.
-#' @param string String to count letters in. Defaults to "foo"
+#' @param path String to count letters in. Defaults to "foo"
 #' @export
 #' @examples
 #' read_biokin()
 
-read_biokine <- function(string = "foo"){
-  return(nchar(string))
+read_biokine <- function(path = ""){
+  comment_lines <- read_lines(path, n_max = 100) %>%
+    str_detect("\"")
+  
+  comment_lines <- max(which(comment_lines))
+  
+  data <- read_tsv(file = path, skip = comment_lines, col_names = c("time", "value"))
+  
+  return(data)
 }
